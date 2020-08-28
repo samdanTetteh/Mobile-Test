2f2084ed649bfc8280f5a9de59a7476b25a75cfa
samdan <samdan@whizzogroup.com>
2020-08-28 16:37:28 +0100

    implemented room local database

---
+++ app/src/main/java/com/ijikod/uni/data/Room/UniDatabase.kt
@@ -0,0 +1,41 @@
+package com.ijikod.uni.data.Room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.ijikod.uni.data.Model.UniModel
+
+/**
+ * Room implementation of [UniDatabase] to contain entities
+ * **/
+@Database(entities = arrayOf(UniModel::class), version = 1, exportSchema = false)
+abstract class UniDatabase : RoomDatabase(){
+
+    /**
+     * [UniDao] instance to access from repository
+     * **/
+    abstract fun dao() : UniDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: UniDatabase? = null
+
+        /**
+         * Singleton implementation of room database to avoid creation of several instances
+         * **/
+        fun getDatabase(context: Context): UniDatabase {
+            if (INSTANCE == null) {
+                synchronized(this){
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        UniDatabase::class.java,
+                        "Unidays.db"
+                    ).build()
+                }
+            }
+            return  INSTANCE!!
+        }
+    }
+
+}
