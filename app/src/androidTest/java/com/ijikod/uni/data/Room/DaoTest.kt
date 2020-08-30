5a9003aca44469bdf3ff8ce9547d5f2d58dfa8a3
samdan <samdan@whizzogroup.com>
2020-08-30 07:27:24 +0100

    added tests for local database

--- app/src/androidTest/java/com/ijikod/uni/data/Room/DaoTest.kt
+++ app/src/androidTest/java/com/ijikod/uni/data/Room/DaoTest.kt
@@ -5,7 +5,9 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.ijikod.uni.data.Model.UniModel
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 
 
@@ -13,6 +15,18 @@ import org.junit.runner.RunWith
 class DaoTest {
 
     private lateinit var appDatabase: UniDatabase
+
+    private val fakeUniModel_1  = UniModel(
+        title = "Get 35% Student Discount",
+        description = "Plus save an extra 30% off on Outlet items. Hurry Limited Time Only!",
+        entity = "Reebok"
+    )
+
+    private val fakeUniModel_2  = UniModel(
+        title = "22% Student Discount",
+        description = "Head to LOOKFANTASTIC - the ultimate destination for your beauty needs!",
+        entity = "LOOKFANTASTIC"
+    )
 
     /**
      * In memory implementation of database for tests
@@ -24,6 +38,44 @@ class DaoTest {
     }
 
 
+    /**
+     * Check local saving of data
+     * **/
+    @Test
+    fun `check_insert_localData`(){
+        val dataItem = fakeUniModel_1
+        appDatabase.dao().insertData(dataItem)
+        val cachedData = appDatabase.dao().getAllData()
+        assert(cachedData.isNotEmpty())
+    }
 
+    /**
+     * Check retrieval of locally saved data
+     * **/
+    @Test
+    fun `check_retrieval_of_data`(){
+        val dataList  = listOf(fakeUniModel_1, fakeUniModel_2)
+        appDatabase.dao().insertAllData(dataList)
+
+        val retrievedData = appDatabase.dao().getAllData()
+        // Check if data retrieved is the same inserted.
+        assert(retrievedData == dataList.sortedWith(compareBy({ it.entity }, { it.entity })))
+    }
+
+
+    /**
+     * Check deleting data from local database
+     * **/
+    @Test
+    fun `check_deletion_of_data`(){
+        val dataList  = listOf(fakeUniModel_1, fakeUniModel_2)
+        dataList.forEach {
+            appDatabase.dao().insertData(it)
+        }
+
+        appDatabase.dao().deleteAll()
+        val dataRetrieved = appDatabase.dao().getAllData()
+        assert(dataRetrieved.isEmpty())
+    }
 
 }
